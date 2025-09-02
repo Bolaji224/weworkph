e91e4c61a4f9ec509d5ecbd5a330cc04a019f7e9
Mustarphy <abdulazeezmustapha204@gmail.com>
2025-09-01 17:52:27 -0700

    added our user section

--- src/pages/about/About.tsx
+++ src/pages/about/About.tsx
@@ -3,12 +3,14 @@ import AboutHeroSection from './components/AboutHeroSection'
 import RecruitPionerSection from './components/RecruitPionerSection'
 import WhatTheySayingSection from '../company/components/WhatTheySayingSection'
 import FooterSection from '../../components/reusable/FooterSection'
+import OurUsersSection from './components/OurUserSection'
 
 const About: React.FC = () => {
   return (
     <>
     <AboutHeroSection />
     <RecruitPionerSection />
+    <OurUsersSection/>
     <WhatTheySayingSection />
     <FooterSection />
     </>
